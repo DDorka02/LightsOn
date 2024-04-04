aaa0be00709fcd73549c89fe625eaa8bb56f1763
DDorka02 <dobszaydorka@gmail.com>
2024-04-04 10:43:56 +0200

    kkk

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import { SZINEK } from "./adatok.js";
 
+function letrehozTAblazat(tomb){
+    let txt = "<table>"
+    for (let index = 0; index < tomb.length; index++) {
+        txt += "<tr>"
+        txt += "<td>"+GYUMOLCSOK[index].nev+"</td>"
+        txt += "<td>"+GYUMOLCSOK[index].tomeg+"</td>"
+        txt += "<td>"+GYUMOLCSOK[index].ar+"</td>"
+        txt += "</tr>"  
+    }
+    txt += "</table>"
+    console.log(txt)
+
+    return txt
+
+}
