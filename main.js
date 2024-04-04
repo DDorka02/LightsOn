ff773fa9424eef35f492546021532d60840c0c33
DDorka02 <dobszaydorka@gmail.com>
2024-04-04 10:59:53 +0200

    background-color

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import { SZINEK } from "./adatok.js";
 
-function letrehozTablazat(tomb){
-        let txt = `<div class="kepek">`
-        txt += `<img src="red.png" alt="piros">`
-        txt += `<img src="blue.png" alt="kek">`
+function letrehozTablazat(lista){
+        let txt = `<div class="kepek style="background-color:${lista[i]}">`
+        txt += ``
+        txt += ``
         txt += "</div>"
         console.log(txt)
 
@@ -12,4 +12,4 @@ function letrehozTablazat(tomb){
 }
 
 const ELEM = $(".lista")
-ELEM
+ELEM.html=letrehozTablazat(SZINEK)
