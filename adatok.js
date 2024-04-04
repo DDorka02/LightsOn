3465aba3473f3b5dad52afcb6d5101d648541caa
Fabu András <fabuandras@gmail.com>
2024-04-04 11:00:21 +0200

    adatok feltoltese2

--- adatok.js
+++ adatok.js
@@ -1,4 +1,11 @@
 export const SZINEK = [
     { szin: 'red'},
-    { szin: 'blue'}
+    { szin: 'blue'},
+    { szin: 'red'},
+    { szin: 'blue'},
+    { szin: 'red'},
+    { szin: 'blue'},
+    { szin: 'red'},
+    { szin: 'blue'},
+    { szin: 'red'}
 ];
