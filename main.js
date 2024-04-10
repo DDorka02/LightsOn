80191e243b22dfec3d4cbd1fca6a74dd99c6e99e
DDorka02 <dobszaydorka@gmail.com>
2024-04-10 22:51:35 +0200

    tablazat megpróbálásának létrehozása

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 import { SZINEK } from "./adatok.js";
 
+//const SZINEK =  []
+
+/*function tablazatOsszeallit(){
+    for (let index = 0; index < SZINEK.length; index++) {
+        SZINEK[index] = Math.random()*100;
+        if (SZINEK[index] < 20) {
+            korok = `red`
+        }
+        else {
+            korok = `blue`;
+        }        
+    }
+}*/
+
 function letrehozTablazat(lista){
     let txt = ""
     for (let index = 0; index < lista.length; index++) {
@@ -16,9 +30,10 @@ const ELEM = $(".lista")
 ELEM.html(letrehozTablazat(SZINEK))
 
 
-function szinValtozas(lista) {
-    const szinValt = $(".kepek")
 
+/*function szinValtozas(lista) {
+    const szinValt = $(".kepek")
+        
     szinValt.on("click",function(){
     console.log (event.target.id)
     })
@@ -27,19 +42,24 @@ function szinValtozas(lista) {
     
 
 }
-const ELEM2 = $(".lista")
-ELEM2.html=szinValtozas(SZINEK)
-
-
-/*
+const ELEM2= $(".lista")
+ELEM2.html=szinValtozas()*/
 
 function listaOsszeAllit(tomb) {
+    tomb = []
+    for (let index = 0; index < SZINEK.length; index++) {
+        if (SZINEK[index].szin = 'red') {
+            SZINEK.szin = '<br>'
+        }
+        else {
+            SZINEK.szin = ''
+        }        
+    }
     return tomb
 }
-
 const ELEM3 = $(".lista")
 ELEM3.html=listaOsszeAllit(SZINEK)
-
+/*
 function lekapcsoltLampakSzama(lista) {
     return szam
 }
